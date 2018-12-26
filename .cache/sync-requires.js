484ce0dc7e361e689cb080d1771bce7bb316cb50
Ryan Siu <rsiu05@gmail.com>
2018-12-26 15:21:22 -0500

    Updates to about and website

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -3,9 +3,8 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---node-modules-gatsby-plugin-offline-app-shell-js": preferDefault(require("/home/ryan/dev/siuryan.github.io/node_modules/gatsby-plugin-offline/app-shell.js")),
+  "component---cache-dev-404-page-js": preferDefault(require("/home/ryan/dev/siuryan.github.io/.cache/dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("/home/ryan/dev/siuryan.github.io/src/pages/404.js")),
-  "component---src-pages-index-js": preferDefault(require("/home/ryan/dev/siuryan.github.io/src/pages/index.js")),
-  "component---src-pages-page-2-js": preferDefault(require("/home/ryan/dev/siuryan.github.io/src/pages/page-2.js"))
+  "component---src-pages-index-js": preferDefault(require("/home/ryan/dev/siuryan.github.io/src/pages/index.js"))
 }
 
--- src/markdown/about.md
+++ src/markdown/about.md
@@ -1,3 +1,18 @@
 <span class="image main"><img src="../images/me.jpg" alt="" /></span>
 
 I'm a software developer from NYC currently attending the University of Michigan.
+
+I work with:
+- HTML5/CSS3/Javascript
+- React.js
+- GatsbyJS
+- Jekyll
+- Python
+- SQL
+- DigitalOcean
+- Git
+- Ubuntu
+- Android
+- Java
+- C/C++
+- Arduino
