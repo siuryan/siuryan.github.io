79ead82149b8d3f3ad42a96930f131204ec6c90f
Ryan Siu <rsiu05@gmail.com>
2018-12-26 14:50:21 -0500

    Updates

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/home/ryan/dev/siuryan.github.io/node_modules/gatsby-plugin-offline/gatsby-browser'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('/home/ryan/dev/siuryan.github.io/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
--- src/markdown/about.md
+++
@@ -1,3 +0,0 @@
-<span class="image main"><img src="../images/me.jpg" alt="" /></span>
-
-I'm a software developer from NYC currently attending the University of Michigan.
