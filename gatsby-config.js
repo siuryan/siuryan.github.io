6badef270ea544e0e0bc083039ebecc77f1c0caa
Ryan Siu <rsiu05@gmail.com>
2018-12-27 16:32:23 -0500

    Edit config

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
       options: {
         trackingId: "UA-131461631-1",
         // Puts tracking script in the head instead of the body
-        head: false,
+        head: true,
         // Setting this parameter is optional
         anonymize: true,
         // Setting this parameter is also optional
@@ -33,8 +33,8 @@ module.exports = {
         // Enables Google Optimize using your container Id
         //optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
         // Any additional create only fields (optional)
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
+        //sampleRate: 5,
+        //siteSpeedSampleRate: 10,
         //cookieDomain: "example.com",
       }
     }
