60962d936000e5ce18020d7b84d56a6436ec75b1
Ryan Siu <rsiu05@gmail.com>
2018-12-27 16:10:14 -0500

    Add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,24 @@ module.exports = {
       },
     },
     'gatsby-plugin-sass',
-    'gatsby-plugin-offline'
+    'gatsby-plugin-offline',
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-131461631-1",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Enables Google Optimize using your container Id
+        //optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        // Any additional create only fields (optional)
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        //cookieDomain: "example.com",
+      }
+    }
   ],
 }
