5417b1531d13bd93ffbacea6d8035a1c9b0cab4f
Ryan Siu <rsiu05@gmail.com>
2022-09-11 18:47:01 -0400

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-752eee71a27654276ba5.js"
+    "url": "webpack-runtime-e232953ec97d5c170383.js"
   },
   {
     "url": "framework-dbb498007a7447f28d8e.js"
@@ -39,7 +39,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "3b57fbcacb65eba8e4e3cb6f1226eb70"
+    "revision": "5e62fcb18b071ff21e63b7fd5d6d1480"
   },
   {
     "url": "polyfill-ca8adf9770cfeec4c092.js"
